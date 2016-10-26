16286db2489fa0f4eaaf7304e93cdca7e92a8afb
Dionisius <maslo111@rambler.ru>
2016-10-26 23:42:15 +0300

    Add files via upload

---
+++ chapter_001/arrays_5/src/test/java/ru/dionisius/QuadrateArrayTest.java
@@ -0,0 +1,20 @@
+package ru.dionisius;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class QuadrateArrayTest {
+	@Test
+	public void arrayRotationTest(){
+		int[][] initialArray={{1,2,3,4},{1,2,3,4},{1,2,3,4},{1,2,3,4}};
+		int[][] expectedArray={{1,1,1,1},{2,2,2,2},{3,3,3,3},{4,4,4,4}};
+		QuadrateArray quadrateArray=new QuadrateArray(initialArray);
+		quadrateArray.rotate();
+		for (int i=0; i<initialArray.length; i++){
+			for (int j=0; j<initialArray.length; j++){
+				assertThat(expectedArray[i][j], is(quadrateArray.quadrateArray[i][j]));
+			}
+		}
+	}
+}

b98c45f647bc2a4a90a07f01de3e9d1c6e38fd86
dionisius1976 <maslo111@rambler.ru>
2016-11-23 12:54:02 +0300

    Palindrome 5 characters check added

--- chapter_003/inputOutput_1/src/main/java/ru/dionisius/Palindrome.java
+++ chapter_003/inputOutput_1/src/main/java/ru/dionisius/Palindrome.java
@@ -9,33 +9,42 @@ public class Palindrome {
 
     /**
      * method isPalinrome(), checks if the word is palindrome.
+     *
      * @param string - checking word
      * @return true if word is palindrome, false if not
      */
-        public boolean isPalindrome (String string) {
-            boolean isPalindrome = true;
-            string = string.toLowerCase();
-            for (int i = 0; i < string.length() /2; i++) {
-                if (string.charAt(i) != string.charAt(string.length() - 1 - i)) {
-                    isPalindrome = false;
-                    break;
-                }
+    public boolean isPalindrome(String string) {
+        boolean isPalindrome = true;
+        string = string.toLowerCase();
+        for (int i = 0; i < string.length() / 2; i++) {
+            if (string.charAt(i) != string.charAt(string.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
             }
-            return isPalindrome;
         }
+        return isPalindrome;
+    }
 
     /**
      * Main method. Runs the program.
      * Get inputed in console by user word
      * and prints true if inputed word is palindrome
      * and false if inputed word is not palindrome.
+     *
      * @param args inputed in console word
      */
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            ru.dionisius.Palindrome palindrome = new ru.dionisius.Palindrome();
-            System.out.print("Input testing word: ");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ru.dionisius.Palindrome palindrome = new ru.dionisius.Palindrome();
+        while (true) {
+            System.out.print("Input five characters testing word: ");
             String string = scanner.nextLine();
+            if (string.length() != 5) {
+                System.out.println("It's not five characters word!");
+                continue;
+            }
             System.out.println(palindrome.isPalindrome(string));
+            break;
         }
+    }
 }

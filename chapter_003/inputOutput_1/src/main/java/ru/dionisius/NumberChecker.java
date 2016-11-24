d0f9715e295349aa6924d46d2c5a0c2f5d0c539a
dionisius1976 <maslo111@rambler.ru>
2016-11-24 16:48:24 +0300

    Number checker try block modified sb

--- chapter_003/inputOutput_1/src/main/java/ru/dionisius/NumberChecker.java
+++ chapter_003/inputOutput_1/src/main/java/ru/dionisius/NumberChecker.java
@@ -16,10 +16,11 @@ public class NumberChecker {
      * @return true if count of bytes in InputStream is even or false if count of bytes in InputStream is odd
      */
     public boolean isNumber(InputStream i) {
+        StringBuilder sb
         int c;
         try {
             BufferedInputStream bis = new BufferedInputStream(i);
-            StringBuilder sb = new StringBuilder();
+            sb = new StringBuilder();
             while ((c = bis.read()) != -1) {
                 sb.append((char) c);
             }

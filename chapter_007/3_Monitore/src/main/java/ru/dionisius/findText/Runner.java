43df32115d39cdd5a041115b552c831b56271ccd
dionisius1976 <maslo111@rambler.ru>
2017-03-15 10:28:20 +0300

    multithread  text finder Runner added

---
+++ chapter_007/3_Monitore/src/main/java/ru/dionisius/findText/Runner.java
@@ -0,0 +1,15 @@
+package ru.dionisius.findText;
+
+/**
+ * Created by Dionisius on 15.03.2017.
+ * Runs the program.
+ */
+public class Runner {
+    /**
+     * Starts the program.
+     * @param args console arguments.
+     */
+    public static void main(String[] args) {
+        new Find(args).init();
+    }
+}

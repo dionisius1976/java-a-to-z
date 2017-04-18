35737f935a37889905485e7baa24d468d0894dad
dionisius1976 <maslo111@rambler.ru>
2017-04-18 22:41:14 +0300

    SQL first task is fixed

--- chapter_008/1_UML/createTables.sql
+++ chapter_008/1_UML/createTables.sql
@@ -1,54 +1,54 @@
-CREATE TABLE orders (
-	id serial primary key,
-	name varchar(50),
-	create_date timestamp,
-	user_id integer references users(id),
-	categorie_id integer references order_categories(id),
-	comment_id integer references order_comments(id),
-	state_id integer references order_states(id)
-);
-
 CREATE TABLE order_comments (
-	id serial primary key,
-	name text,
+	id SERIAL PRIMARY KEY,
+	name TEXT,
 	create_date timestamp
 );
 
 CREATE TABLE order_states (
-	id serial primary key,
-	name varchar(50)
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50)
 );
 
 CREATE TABLE order_categories (
-	id serial primary key,
-	name varchar(50)
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50)
 );
 
 CREATE TABLE order_files (
-	id serial primary key,
-	name varchar(50),
-	content bytea,
-	order_id integer references orders(id)
-);
-
-CREATE TABLE users (
-	id serial primary key,
-	name varchar(50),
-	user_role_id integer references user_roles
-	
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50),
+	content BYTEA
 );
 
 CREATE TABLE user_roles (
-	id serial primary key,
-	name varchar(50)
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50)
+);
+
+
+CREATE TABLE permissions (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50)
 );
 
 CREATE TABLE role_permissions (
-	role_id integer references user_roles(id),
-	permission_id integer references permissions(id)
+	role_id INTEGER REFERENCES user_roles(id),
+	permission_id INTEGER REFERENCES permissions(id)
 );
 
-CREATE TABLE permissions (
-	id serial primary key,
-	name varchar(50)
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50),
+	user_role_id INTEGER REFERENCES user_roles(id)
+);
+
+CREATE TABLE orders (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50),
+	create_date TIMESTAMP,
+	user_id INTEGER REFERENCES users(id),
+	categorie_id INTEGER REFERENCES order_categories(id),
+	comment_id INTEGER REFERENCES order_comments(id),
+	state_id INTEGER REFERENCES order_states(id),
+	order_file_id INTEGER REFERENCES order_files(id)
 );
--- chapter_008/1_UML/init.sql
+++ chapter_008/1_UML/init.sql
@@ -11,15 +11,6 @@ INSERT INTO order_states (name) VALUES ('Closed');
 INSERT INTO order_categories (name) VALUES ('Bug');
 INSERT INTO order_categories (name) VALUES ('Task');
 
-INSERT INTO orders (name ,create_date, user_id, categorie_id, comment_id,
-	state_id, files_id) VALUES ('Task from admin', current_timestamp, '1', '2', '1');
-INSERT INTO orders (name ,create_date, user_id, categorie_id, comment_id,
-	state_id, files_id) VALUES ('Bug from admin', current_timestamp, '1', '1', '3');
-INSERT INTO orders (name ,create_date, user_id, categorie_id, comment_id,
-	state_id, files_id) VALUES ('Task from seller', current_timestamp, '2', '2', '2');
-INSERT INTO orders (name ,create_date, user_id, categorie_id, comment_id,
-	state_id, files_id) VALUES ('Bug from buyer', current_timestamp, '5', '1', '4');
-
 INSERT INTO user_roles (name) VALUES ('Admin');
 INSERT INTO user_roles (name) VALUES ('Seller');
 INSERT INTO user_roles (name) VALUES ('Buyer');
@@ -28,16 +19,27 @@ INSERT INTO permissions (name) VALUES ('Search');
 INSERT INTO permissions (name) VALUES ('Copy');
 INSERT INTO permissions (name) VALUES ('Edit');
 
-INSERT INTO role_permissions (role_id, permission_id) VALUES ('1,1');
-INSERT INTO role_permissions (role_id, permission_id) VALUES ('1,2');
-INSERT INTO role_permissions (role_id, permission_id) VALUES ('1,3');
-INSERT INTO role_permissions (role_id, permission_id) VALUES ('2,1');
-INSERT INTO role_permissions (role_id, permission_id) VALUES ('2,2');
-INSERT INTO role_permissions (role_id, permission_id) VALUES ('3,1');
+INSERT INTO users (name, user_role_id) VALUES ('Ivanov',1);
+INSERT INTO users (name, user_role_id) VALUES ('Petrov',2);
+INSERT INTO users (name, user_role_id) VALUES ('Sidorov',2);
+INSERT INTO users (name, user_role_id) VALUES ('Kolosov',3);
+INSERT INTO users (name, user_role_id) VALUES ('Alexeev',3);
 
-INSERT INTO users (name, user_role_id) VALUES ('Ivanov','1');
-INSERT INTO users (name, user_role_id) VALUES ('Petrov','2');
-INSERT INTO users (name, user_role_id) VALUES ('Sidorov','2');
-INSERT INTO users (name, user_role_id) VALUES ('Kolosov','3');
-INSERT INTO users (name, user_role_id) VALUES ('Alexeev','3');
+INSERT INTO role_permissions (role_id, permission_id) VALUES (1,1);
+INSERT INTO role_permissions (role_id, permission_id) VALUES (1,2);
+INSERT INTO role_permissions (role_id, permission_id) VALUES (1,3);
+INSERT INTO role_permissions (role_id, permission_id) VALUES (2,1);
+INSERT INTO role_permissions (role_id, permission_id) VALUES (2,2);
+INSERT INTO role_permissions (role_id, permission_id) VALUES (3,1);
+
+INSERT INTO orders (name, create_date, user_id, categorie_id, comment_id,
+	state_id, order_file_id) VALUES ('Task from admin', current_timestamp, 1, 2, 1, 1, 1);
+INSERT INTO orders (name, create_date, user_id, categorie_id, comment_id,
+	state_id, order_file_id) VALUES ('Bug from admin', current_timestamp, 1, 1, 3, 2, 1);
+INSERT INTO orders (name, create_date, user_id, categorie_id, comment_id,
+	state_id, order_file_id) VALUES ('Task from seller', current_timestamp, 2, 2, 2, 3, 2);
+INSERT INTO orders (name, create_date, user_id, categorie_id, comment_id,
+	state_id, order_file_id) VALUES ('Bug from buyer', current_timestamp, 5, 1, 4, 4, 3);
+
+
 

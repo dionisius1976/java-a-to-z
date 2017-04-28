7d7cf48059da2e88b9d4ec528485f684d4e0a46c
dionisius1976 <maslo111@rambler.ru>
2017-04-28 06:54:51 +0300

    SQL first task many comments ability is added

--- chapter_008/1_UML/createTables.sql
+++ chapter_008/1_UML/createTables.sql
@@ -1,7 +1,22 @@
-CREATE TABLE order_comments (
+CREATE TABLE user_roles (
 	id SERIAL PRIMARY KEY,
-	name TEXT,
-	create_date timestamp
+	name VARCHAR(50)
+);
+
+CREATE TABLE user_permissions (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50)
+);
+
+CREATE TABLE role_permissions (
+	role_id INTEGER REFERENCES user_roles(id),
+	user_permission_id INTEGER REFERENCES user_permissions(id)
+);
+
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50),
+	user_role_id INTEGER REFERENCES user_roles(id)
 );
 
 CREATE TABLE order_states (
@@ -20,35 +35,19 @@ CREATE TABLE order_files (
 	content BYTEA
 );
 
-CREATE TABLE user_roles (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(50)
-);
-
-
-CREATE TABLE permissions (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(50)
-);
-
-CREATE TABLE role_permissions (
-	role_id INTEGER REFERENCES user_roles(id),
-	permission_id INTEGER REFERENCES permissions(id)
-);
-
-CREATE TABLE users (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(50),
-	user_role_id INTEGER REFERENCES user_roles(id)
-);
-
 CREATE TABLE orders (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(50),
 	create_date TIMESTAMP,
 	user_id INTEGER REFERENCES users(id),
 	categorie_id INTEGER REFERENCES order_categories(id),
-	comment_id INTEGER REFERENCES order_comments(id),
 	state_id INTEGER REFERENCES order_states(id),
 	order_file_id INTEGER REFERENCES order_files(id)
+);
+
+CREATE TABLE oredr_comments (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	create_date timestamp,
+	order_id INTEGER REFERENCES orders(id)
 );
--- chapter_008/1_UML/init.sql
+++ chapter_008/1_UML/init.sql
@@ -1,7 +1,23 @@
-INSERT INTO order_comments (name, create_date) VALUES ('Urgent', current_timestamp);
-INSERT INTO order_comments (name, create_date) VALUES ('Common', current_timestamp);
-INSERT INTO order_comments (name, create_date) VALUES ('Important', current_timestamp);
-INSERT INTO order_comments (name, create_date) VALUES ('Unimportant', current_timestamp);
+INSERT INTO user_roles (name) VALUES ('Admin');
+INSERT INTO user_roles (name) VALUES ('Seller');
+INSERT INTO user_roles (name) VALUES ('Buyer');
+
+INSERT INTO user_permissions (name) VALUES ('Search');
+INSERT INTO user_permissions (name) VALUES ('Copy');
+INSERT INTO user_permissions (name) VALUES ('Edit');
+
+INSERT INTO role_permissions (role_id, user_permission_id) VALUES (1,1);
+INSERT INTO role_permissions (role_id, user_permission_id) VALUES (1,2);
+INSERT INTO role_permissions (role_id, user_permission_id) VALUES (1,3);
+INSERT INTO role_permissions (role_id, user_permission_id) VALUES (2,1);
+INSERT INTO role_permissions (role_id, user_permission_id) VALUES (2,2);
+INSERT INTO role_permissions (role_id, user_permission_id) VALUES (3,1);
+
+INSERT INTO users (name, user_role_id) VALUES ('Ivanov',1);
+INSERT INTO users (name, user_role_id) VALUES ('Petrov',2);
+INSERT INTO users (name, user_role_id) VALUES ('Sidorov',2);
+INSERT INTO users (name, user_role_id) VALUES ('Kolosov',3);
+INSERT INTO users (name, user_role_id) VALUES ('Alexeev',3);
 
 INSERT INTO order_states (name) VALUES ('Created');
 INSERT INTO order_states (name) VALUES ('Pending');
@@ -11,35 +27,25 @@ INSERT INTO order_states (name) VALUES ('Closed');
 INSERT INTO order_categories (name) VALUES ('Bug');
 INSERT INTO order_categories (name) VALUES ('Task');
 
-INSERT INTO user_roles (name) VALUES ('Admin');
-INSERT INTO user_roles (name) VALUES ('Seller');
-INSERT INTO user_roles (name) VALUES ('Buyer');
-
-INSERT INTO permissions (name) VALUES ('Search');
-INSERT INTO permissions (name) VALUES ('Copy');
-INSERT INTO permissions (name) VALUES ('Edit');
-
-INSERT INTO users (name, user_role_id) VALUES ('Ivanov',1);
-INSERT INTO users (name, user_role_id) VALUES ('Petrov',2);
-INSERT INTO users (name, user_role_id) VALUES ('Sidorov',2);
-INSERT INTO users (name, user_role_id) VALUES ('Kolosov',3);
-INSERT INTO users (name, user_role_id) VALUES ('Alexeev',3);
-
-INSERT INTO role_permissions (role_id, permission_id) VALUES (1,1);
-INSERT INTO role_permissions (role_id, permission_id) VALUES (1,2);
-INSERT INTO role_permissions (role_id, permission_id) VALUES (1,3);
-INSERT INTO role_permissions (role_id, permission_id) VALUES (2,1);
-INSERT INTO role_permissions (role_id, permission_id) VALUES (2,2);
-INSERT INTO role_permissions (role_id, permission_id) VALUES (3,1);
-
-INSERT INTO orders (name, create_date, user_id, categorie_id, comment_id,
-	state_id, order_file_id) VALUES ('Task from admin', current_timestamp, 1, 2, 1, 1, 1);
-INSERT INTO orders (name, create_date, user_id, categorie_id, comment_id,
-	state_id, order_file_id) VALUES ('Bug from admin', current_timestamp, 1, 1, 3, 2, 1);
-INSERT INTO orders (name, create_date, user_id, categorie_id, comment_id,
-	state_id, order_file_id) VALUES ('Task from seller', current_timestamp, 2, 2, 2, 3, 2);
-INSERT INTO orders (name, create_date, user_id, categorie_id, comment_id,
-	state_id, order_file_id) VALUES ('Bug from buyer', current_timestamp, 5, 1, 4, 4, 3);
-
+INSERT INTO orders (name, create_date, user_id, categorie_id,
+	state_id) VALUES ('Task from admin', current_timestamp, 1, 2, 1);
+INSERT INTO orders (name, create_date, user_id, categorie_id,
+	state_id) VALUES ('Bug from admin', current_timestamp, 1, 1, 2);
+INSERT INTO orders (name, create_date, user_id, categorie_id,
+	state_id) VALUES ('Task from seller', current_timestamp, 2, 2, 3);
+INSERT INTO orders (name, create_date, user_id, categorie_id,
+	state_id) VALUES ('Bug from buyer', current_timestamp, 3, 1, 4);
+	
+INSERT INTO order_files (name, content, order_id) VALUES ('Task', NULL, 1);
+INSERT INTO order_files (name, content, order_id) VALUES ('Data', NULL, 1);
+INSERT INTO order_files (name, content, order_id) VALUES ('Description', NULL, 2);
+INSERT INTO order_files (name, content, order_id) VALUES ('Seller_task', NULL, 3);
+INSERT INTO order_files (name, content, order_id) VALUES ('Bug_description', NULL, 4);
+	
+INSERT INTO order_comments (name, create_date, order_id) VALUES ('Urgent task!', current_timestamp, 1);
+INSERT INTO order_comments (name, create_date, order_id) VALUES ('See additional data in attached files.', current_timestamp, 1);
+INSERT INTO order_comments (name, create_date, order_id) VALUES ('Common seller task.', current_timestamp, 3);
+INSERT INTO order_comments (name, create_date, order_id) VALUES ('Must be fixed.', current_timestamp, 2);
+INSERT INTO order_comments (name, create_date, order_id) VALUES ('Must be fixed.', current_timestamp, 4);
 
 
